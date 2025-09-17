a41a5385025d382b8cf2595858d2b6e049bc09b6
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-17 12:53:46 +0200

    Initial Home Page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,33 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
+const font = Poppins({ subsets: ["latin"], weight: ["600"] });
 
 export default function Home() {
-  return <Button size={"lg"}>Click me</Button>;
+  return (
+    <main
+      style={{
+        background: "radial-gradient(ellipse at top, #38bdf8, #1e3a8a)",
+      }}
+      className="h-full flex flex-col items-center justify-center"
+    >
+      <div className={"space-y-6 text-center"}>
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className={"text-white text-lg"}>A simple authentication service</p>
+        <div>
+          <Button variant={"secondary"} size={"lg"}>
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
