d27b522a7f903a441c91c13d1395eb73c7639d05
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-17 18:21:04 +0200

    Reusable Card component for LOGIN and REGISTER users

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,5 @@
+import LoginForm from "@/components/auth/login-form";
+
 export default function LoginPage() {
-  return (
-    <div style={{ backgroundColor: "orangered", color: "white" }}>
-      Login page
-    </div>
-  );
+  return <LoginForm />;
 }
