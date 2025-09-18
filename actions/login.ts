30586cd0de1155aea0e0eef131c30eaaf4f77597
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-18 23:03:07 +0200

    Authentication with signIn() auth.js method

--- actions/login.ts
+++ actions/login.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "@auth/core/errors";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -9,5 +12,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   if (!validatedFields.success)
     return { error: "Invalid fields! Deder popravi!" };
 
-  return { success: "Logged in successfully! Kralju!!!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credential (email or password)" };
+        default:
+          return { error: "Something went wrong during login" };
+      }
+    }
+    throw error;
+  }
 };
