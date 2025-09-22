93e9f737d25226af9e5b928ac7c1b5a79f52564a
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-22 12:14:54 +0200

    Login MODAL window

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         </h1>
         <p className={"text-white text-lg"}>A simple authentication service</p>
         <div>
-          <LoginButton>
+          <LoginButton mode={"modal"} asChild={true}>
             <Button variant={"secondary"} size={"lg"}>
               Sign in
             </Button>
--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -2,6 +2,13 @@
 
 import { ReactNode } from "react";
 import { useRouter } from "next/navigation";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import LoginForm from "@/components/auth/login-form";
 
 interface LoginButtonProps {
   children: ReactNode;
@@ -20,7 +27,16 @@ export const LoginButton = ({
     router.push("/auth/login");
   };
 
-  if (mode === "modal") return <span>TODO: Implement modal</span>;
+  if (mode === "modal")
+    return (
+      <Dialog>
+        <DialogTrigger asChild={asChild}>{children}</DialogTrigger>
+        <DialogContent className={"p-0 w-auto bg-transparent border-none"}>
+          <DialogTitle>LOGIN FORM</DialogTitle>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    );
 
   return (
     <span className={"cursor-pointer"} onClick={onClick}>
